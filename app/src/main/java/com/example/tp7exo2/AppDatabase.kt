5f6095119429d4b3713cd2071f78c6b3a679601e
BadreddineZatout <hb_zatout@esi.dz>
2021-05-20 18:43:45 +0100

    room database init

---
+++ app/src/main/java/com/example/tp7exo2/AppDatabase.kt
@@ -0,0 +1,8 @@
+package com.example.tp7exo2
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Doctor::class, Booking::class, Treatment::class), version = 1)
+abstract class AppDatabase:RoomDatabase() {
+}
---
+++ app/src/main/java/com/example/tp7exo2/RoomService.kt
@@ -0,0 +1,11 @@
+package com.example.tp7exo2
+
+import android.content.Context
+import androidx.room.Room
+
+object RoomService {
+    lateinit var context:Context
+    val appDatabase:AppDatabase by lazy{
+        Room.databaseBuilder(context, AppDatabase::class.java, "name").allowMainThreadQueries().build()
+    }
+}
