98f41af857a3bb1aa3a57e35967d0fac6feef49b
BadreddineZatout <hb_zatout@esi.dz>
2021-05-20 20:00:09 +0100

    bdd ready (with type conversion)

--- app/src/main/java/com/example/tp7exo2/AppDatabase.kt
+++ app/src/main/java/com/example/tp7exo2/AppDatabase.kt
@@ -2,8 +2,10 @@ package com.example.tp7exo2
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(entities = arrayOf(Doctor::class, Booking::class, Treatment::class), version = 1)
+@TypeConverters(Converters::class)
 abstract class AppDatabase:RoomDatabase() {
     abstract fun getDoctorDao():DoctorDao
     abstract fun getBookingDao():BookingDao
