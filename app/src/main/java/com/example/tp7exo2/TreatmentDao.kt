81c14efa64da3f3b90eb965c15e452b10250beec
BadreddineZatout <hb_zatout@esi.dz>
2021-05-20 21:10:01 +0100

    treatment methods

--- app/src/main/java/com/example/tp7exo2/TreatmentDao.kt
+++ app/src/main/java/com/example/tp7exo2/TreatmentDao.kt
@@ -1,6 +1,7 @@
 package com.example.tp7exo2
 
 import androidx.room.*
+import java.util.*
 
 @Dao
 interface TreatmentDao {
@@ -18,7 +19,7 @@ interface TreatmentDao {
     fun deleteTreatment(treatment: Treatment)
 
     @Query("select * from Treatment where treatmentBeginDate>=:current and treatmentEndDate<=:current")
-    fun getCurrentTreatments(current:Long):List<Treatment>
+    fun getCurrentTreatments(current:Date):List<Treatment>
 
     @Query("select * from treatment natural join booking natural join doctor where lastName like :docName")
     fun getCurrentTreatmentByDoctor(docName:String):List<Treatment>
